f5338eb9e3b34c631231497ac39da37356654bb3
Runar Saur Modahl <runarmod@gmail.com>
2020-10-23 18:24:57 +0200

    First Commit

---
+++ fizzbuzz.js
@@ -0,0 +1,16 @@
+function fizzBuzz(words = ["Fizz", "Buzz"], length = 100) {
+    for (var i = 1; i <= length; i++) {
+        var output = "";
+        for (var j = 0; j < words.length; j++) {
+            if (i % ((j + 1) * 2 + 1) === 0) {
+                output += words[j]
+            }
+        }
+        if (output === "") {
+            output = i;
+        }
+        console.log(output);
+    }
+}
+
+fizzBuzz()
---
+++ fizzbuzz.py
@@ -0,0 +1,12 @@
+def fizzBuzz(words = ["Fizz", "Buzz"], length = 100):
+    for i in range(length):
+        i += 1
+        output = ""
+        for j in range(len(words)):
+            if i % ((j + 1) * 2 + 1) == 0:
+                output += words[j]
+        if output == "":
+            output = i
+        print(output)
+
+fizzBuzz()
